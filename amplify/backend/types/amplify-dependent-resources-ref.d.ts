40552cdc1be577f420538ec809be97259a45d2ed
Patrick Gilmartin <patrickdgilmartin@gmail.com>
2022-06-22 06:30:58 -0600

    reinitialized amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,10 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-    "function": {
-        "enakaToolsLambda": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        }
-    }
-}
+export type AmplifyDependentResourcesAttributes = {}
