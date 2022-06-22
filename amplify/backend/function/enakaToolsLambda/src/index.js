c419282611399fd389a44202a1aa803fa384dead
Patrick Gilmartin <patrickdgilmartin@gmail.com>
2022-06-21 22:32:48 -0600

    reinitialized amplify to fix stack problem

--- amplify/backend/function/enakaToolsLambda/src/index.js
+++ amplify/backend/function/enakaToolsLambda/src/index.js
@@ -10,6 +10,6 @@ const server = awsServerlessExpress.createServer(app);
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = (event, context) => {
-  console.log(`EVENT: ${JSON.stringify(event)}`);
-  return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
+    console.log(`EVENT: ${JSON.stringify(event)}`);
+    return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
 };
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
     "function": {
-        "eveLambda": {
+        "enakaToolsLambda": {
             "Name": "string",
             "Arn": "string",
             "Region": "string",
@@ -8,7 +8,7 @@ export type AmplifyDependentResourcesAttributes = {
         }
     },
     "api": {
-        "eveAPI": {
+        "enakaToolsAPI": {
             "RootUrl": "string",
             "ApiName": "string",
             "ApiId": "string"
